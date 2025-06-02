863bde7ad90d62c7a1a1ad700038cc3e94d607a1
jainaneesh <jainaneesh15@gmail.com>
2025-06-01 21:40:19 -0400

    First commit to this repository

---
+++ FILEIO/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fnctl.h>
+
+
+#define FILE "write.txt"
+
+int main(int argc, char *argv[]){
+
+	// Variable to store user data
+	char user_input[10];
+	
+	// Variable to store file descriptor
+	int fd = 0;
+
+	// Open the file and create if it doesn't exist
+	fd = open(FILE, )
+	
+}
